39417e6b5972a9ae2a36b14330f9add7387169f5
Steven Lin <stevenlin1111@gmail.com>
2019-02-02 23:44:23 -0800

    WIP

--- multiworld/envs/mujoco/__init__.py
+++ multiworld/envs/mujoco/__init__.py
@@ -311,8 +311,42 @@ def register_custom_envs():
             hide_goal_markers=True,
             num_goals_presampled=1000,
         )
-
     )
+    register(
+        id='SawyerPickupEnvYZ-v0',
+        entry_point='multiworld.envs.mujoco.sawyer_xyz'
+                    '.sawyer_pick_and_place:SawyerPickAndPlaceEnvYZ',
+        tags={
+            'git-commit-hash': '30f23f7',
+            'author': 'steven',
+        },
+        kwargs=dict(
+            hand_low=(-0.1, 0.55, 0.05),
+            hand_high=(0.0, 0.65, 0.2),
+            action_scale=0.02,
+            hide_goal_markers=True,
+            num_goals_presampled=1000,
+        )
+    )
+    register(
+        id='SawyerPickupEnvYZEasy-v0',
+        entry_point='multiworld.envs.mujoco.sawyer_xyz'
+                    '.sawyer_pick_and_place:SawyerPickAndPlaceEnvYZ',
+        tags={
+            'git-commit-hash': '30f23f7',
+            'author': 'steven',
+        },
+        kwargs=dict(
+            hand_low=(-0.1, 0.55, 0.05),
+            hand_high=(0.0, 0.65, 0.13),
+            action_scale=0.02,
+            hide_goal_markers=True,
+            num_goals_presampled=1000,
+        )
+    )
+
+
+
 
 
 def create_image_48_sawyer_reach_xy_env_v1():
